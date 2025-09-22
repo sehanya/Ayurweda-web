0bdff89cfdcb6d2f8751f287afe99b3d825c882c
sehanya <it24101643@my.sliit.lk>
2025-09-22 14:08:03 +0530

    add

--- src/main/java/com/example/AyurvedaWeb/AyurvedicWebApplication.java
+++ src/main/java/com/example/AyurvedaWeb/AyurvedicWebApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AyurwedicWebApplication {
+public class AyurvedicWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AyurwedicWebApplication.class, args);
+        SpringApplication.run(AyurvedicWebApplication.class, args);
 
     }
 }
