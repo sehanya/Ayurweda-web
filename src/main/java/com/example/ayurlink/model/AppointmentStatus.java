c99d07b0b0ab3f9914fcd06a01aa49306dd57430
sehanya <it24101643@my.sliit.lk>
2025-10-27 01:53:19 +0530

    add parts

---
+++ src/main/java/com/example/ayurlink/model/AppointmentStatus.java
@@ -0,0 +1,10 @@
+package com.example.ayurlink.model;
+
+public enum AppointmentStatus {
+    PENDING,
+    SCHEDULED,
+    CONFIRMED,
+    COMPLETED,
+    CANCELLED,
+    RESCHEDULED
+}
---
+++ src/main/java/com/example/ayurlink/model/ChargeStatus.java
@@ -0,0 +1,6 @@
+package com.example.ayurlink.model;
+
+public enum ChargeStatus {
+    COLLECTED,  // Clinic charge collected
+    REFUNDED    // If payment was refunded
+}
